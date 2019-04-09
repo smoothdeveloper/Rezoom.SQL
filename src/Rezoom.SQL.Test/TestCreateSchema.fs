94ebc3a5230c5b9fd799f8d53ca1f279ce9f1e5c
gauthier <gauthier.segay@gmail.com>
2019-04-09 15:23:32 -0700

    create schema test, a failing one to start with...

---
+++ src/Rezoom.SQL.Test/TestCreateSchema.fs
@@ -0,0 +1,15 @@
+﻿module Rezoom.SQL.Test.CreateSchema
+open Rezoom.SQL.Compiler
+open NUnit.Framework
+
+[<Test>]
+let ``schema`` () =
+    { defaultTest with
+        Migration = """
+            create schema fancy_schema;
+            create table fancy_schema.fancy_table (fancy_column int not null);
+        """
+        Command = "insert into fancy_schema.fancy_table (fancy_column) values (1)"
+        Expect = Good expect
+    } |> assertSimple
+
